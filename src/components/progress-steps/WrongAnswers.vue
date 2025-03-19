4530ef2cc6e4044552edb54927314e47d60f636d
Krastylev Alex <krastylev@gmail.com>
2025-03-19 10:15:14 +0300

    fixes

--- src/components/progress-steps/WrongAnswers.vue
+++ src/components/progress-steps/WrongAnswers.vue
@@ -57,8 +57,9 @@ function isSelectedAnswer(optionId) {
 									<path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
 								</svg>
 							</div>
-							<div v-else class="key">
-								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor">
+							<div v-else-if="isSelectedAnswer(option.id)" class="key">
+								<svg
+									xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor">
 									<path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
 								</svg>
 							</div>
